c8d85e05530e0492073a227156f4111ea2049aae
jxhnkndl <jkroyston@gmail.com>
2023-08-14 12:28:01 -0400

    create additional animation and nuance to responsive left navbar/sidebar component

--- src/components/FromLeftPartial.jsx
+++ src/components/FromLeftPartial.jsx
@@ -5,11 +5,26 @@ export default function FromLeftPartial() {
 
   return (
     <>
-      <div className="bg-neutral-950 flex justify-between items-center px-4 lg:px-10 py-6 mx-0">
-        <div className="space-y-2 z-10" onClick={() => setIsOpen(!isOpen)}>
+      <div
+        className={`absolute top-0 left-0 right-0 flex justify-center items-center  px-6 lg:px-10 py-4 mx-0 h-20 ${
+          isOpen ? 'bg-transparent' : 'bg-neutral-950'
+        } ease-in-out duration-200`}
+      >
+        <p
+          className={`text-3xl font-bold text-stone-100 ${isOpen && 'hidden'}`}
+        >
+          KNDL
+        </p>
+
+        <div
+          className="absolute left-6 lg:left-10 top-7.5 space-y-2 z-10"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <span
             class={`block w-8 h-0.5 bg-stone-100 ${
-              isOpen ? 'rotate-45 translate-y-2.5' : 'rotate-0 translate-y-0'
+              isOpen
+                ? 'rotate-[135deg] translate-y-2.5'
+                : 'rotate-0 translate-y-0'
             } ease-in-out duration-200`}
           ></span>
           <span
@@ -20,7 +35,7 @@ export default function FromLeftPartial() {
           <span
             class={`block w-8 h-0.5 bg-stone-100 ${
               isOpen
-                ? '-rotate-45 -translate-y-2.5'
+                ? '-rotate-[135deg] -translate-y-2.5'
                 : 'rotate-0 translate-y-0'
             } ease-in-out duration-200`}
           ></span>
@@ -29,27 +44,51 @@ export default function FromLeftPartial() {
 
       <aside
         className={`flex flex-col items-center justify-center h-full w-full md:w-2/5 lg:w-1/3 top-0 left-0 fixed bg-neutral-950 px-4 lg:px-10 py-4 z-0 ${
-          isOpen ? 'translate-x-0 opacity-95' : '-translate-x-full opacity-100'
+          isOpen ? 'translate-x-0 opacity-80' : '-translate-x-full opacity-100'
         } ease-in-out duration-200 `}
       >
         <ul className="w-[75%] text-center">
-          <li className="text-stone-100 text-3xl font-semibold py-2 mb-6">
+          <li
+            className={`text-stone-100 text-3xl font-semibold py-2 mb-6 ${
+              isOpen ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'
+            } ease-out duration-500 delay-[100ms]`}
+          >
             LINK 1
           </li>
-          <li className="text-stone-100 text-3xl font-semibold py-2 mb-6">
-            LINK 2
+          <li
+            className={`text-stone-100 text-3xl font-semibold py-2 mb-6 ${
+              isOpen ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'
+            } ease-out duration-500 delay-[150ms]`}
+          >
+            LINK 1
           </li>
-          <li className="text-stone-100 text-3xl font-semibold py-2 mb-6">
-            LINK 3
+          <li
+            className={`text-stone-100 text-3xl font-semibold py-2 mb-6 ${
+              isOpen ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'
+            } ease-out duration-500 delay-[200ms]`}
+          >
+            LINK 1
           </li>
-          <li className="text-stone-100 text-3xl font-semibold py-2 mb-6">
-            LINK 4
+          <li
+            className={`text-stone-100 text-3xl font-semibold py-2 mb-6 ${
+              isOpen ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'
+            } ease-out duration-500 delay-[250ms]`}
+          >
+            LINK 1
           </li>
-          <li className="text-stone-100 text-3xl font-semibold py-2 mb-6">
-            LINK 5
+          <li
+            className={`text-stone-100 text-3xl font-semibold py-2 mb-6 ${
+              isOpen ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'
+            } ease-out duration-500 delay-[300ms]`}
+          >
+            LINK 1
           </li>
-          <li className="text-stone-100 text-3xl font-semibold py-2 mb-6">
-            LINK 6
+          <li
+            className={`text-stone-100 text-3xl font-semibold py-2 mb-6 ${
+              isOpen ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'
+            } ease-out duration-1000 delay-[350ms]`}
+          >
+            LINK 1
           </li>
         </ul>
       </aside>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,16 @@
-import HeroImage from '../assets/hero-image.jpeg';
+import HeroImage from '../assets/hero-image.jpg';
 
 export default function Hero() {
   return (
     <div
-      className="min-h-screen"
+      className="min-h-screen grid grid-cols-2 lg:grid-cols-3"
       style={{
         backgroundImage: `url(${HeroImage})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
-        backgroundPosition: 'top'
+        backgroundPosition: 'center'
       }}
-    ></div>
+    >
+    </div>
   );
 }
